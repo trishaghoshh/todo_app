1bc27492ec415c1ed7ccb3bbc7b76aa31a86cc2e
trishaghoshh <126244323+trishaghoshh@users.noreply.github.com>
2024-07-04 12:40:21 +0530

    Add files via upload

---
+++ home_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:test/util/todo_tile.dart';
+
+class HomePage extends StatefulWidget{
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+  class _HomePageState extends State<HomePage> {
+
+    //list of todo tasks
+    List toDoList = [
+      ("Make tutorial", false),
+      ("Exercise", false),
+    ];
+
+    void checkBoxChanged(bool? value, int index){
+      setState(() {
+        toDoList[index][1] = !toDoList[index][1];
+      });
+    }
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        title: Text("TO DO"),
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0], 
+            taskCompleted: toDoList[index][1], 
+            onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
+      )
+    );
+  }
+}
---
+++ todo_tile.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class ToDoTile extends StatelessWidget {
+  final String taskName;
+  final bool taskCompleted;
+  Function(bool?)? onChanged;
+  ToDoTile({
+    super.key,
+    required this.taskName,
+    required this.taskCompleted,
+    required this.onChanged,
+    });
+
+  @override
+  Widget build (BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Container(
+        padding: EdgeInsets.all(24),
+        child: Row(
+          children: [
+            Checkbox(value: taskCompleted, onChanged: onChanged),
+            Text("Make tutorial"),
+          ],
+        ),
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(12),
+        ),
+      )
+    );
+  }
+}
